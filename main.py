f53bd37d6ded514eb7a5f6bad92d1944d0df2abf
Paulose Kurian <paulose610@gmail.com>
2025-12-26 13:46:00 +0530

    project initialization

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"S-w: {SCREEN_WIDTH} and S-h: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
